80f2ca9bc7991f2687d64899385bd93abb24c60d
Luis Pedro Mayorga Rodríguez <luispedrom2006@umes.edu.gt>
2026-05-21 18:49:53 -0600

    Se agrego la clase JsonServices en la carpeta Services

---
+++ Services/JsonService.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+
+namespace LAB15_Ejer3_Repaso.Services
+{
+    public class JsonService
+    {
+        public void Guardar<T>(string ruta, List<T> lista)
+        {
+            string json = JsonSerializer.Serialize(lista);
+            File.WriteAllText(ruta, json);
+        }
+
+        public List<T> Leer<T>(string ruta)
+        {
+            if (!File.Exists(ruta))
+            {
+                return new List<T>();
+            }
+
+            string json = File.ReadAllText(ruta);
+
+            List<T>? lista = JsonSerializer.Deserialize<List<T>>(json);
+
+            if (lista == null)
+            {
+                return new List<T>();
+            }
+
+            return lista;
+        }
+    }
+}
